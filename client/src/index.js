57c5426b7d2067de3b12c266431678e52ae476c6
LilTzar <eeoerkeedu@gmail.com>
2022-08-26 15:06:09 -0600

    added chakra and reformated some files, working on layout and homepage, defining styles

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider, DarkMode, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+	config: {
+		initialColorMode: DarkMode,
+	},
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
+	<ChakraProvider theme={theme}>
 		<App />
-	</React.StrictMode>
+	</ChakraProvider>
 );
